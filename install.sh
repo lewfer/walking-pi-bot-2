4c257a6ccfb2cc434c172a8697abbd5009bd9701
Llewelyn <lif@arcwave.co.uk>
2021-02-08 17:21:14 +0000

    update install

--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
-cp update-walking-pi-bot2.sh ~
+cp update-walking-pi-bot-2.sh ~
 cp tcl_robot.service /etc/systemd/system/
 sudo systemctl enable tcl_robot.service
 sudo systemctl start tcl_robot.service
