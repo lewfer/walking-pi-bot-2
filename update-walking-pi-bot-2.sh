d5adab655ab1a13a92a2b9896564dfa8a9c99171
Llewelyn <lif@arcwave.co.uk>
2021-02-10 14:28:43 +0000

    Fixed update script

--- update-walking-pi-bot-2.sh
+++ update-walking-pi-bot-2.sh
@@ -1,7 +1,7 @@
-rm -r -f ~/tcl-temp/* 2> ~/errors
-rm -r -f ~/tcl-temp/.* 2>> ~/errors
-git clone https://github.com/lewfer/walking-pi-bot-2 ~/tcl-temp -q
-cp -r ~/tcl-temp/* ~/tcl/walking-pi-bot-2/
-rm -r -f ~/tcl-temp/* 2>> ~/errors
-rm -r -f ~/tcl-temp/.* 2>> ~/errors
+rm -r -f /home/pi/tcl-temp/* 2> ~/errors
+rm -r -f /home/pi/tcl-temp/.* 2>> ~/errors
+git clone https://github.com/lewfer/walking-pi-bot-2 /home/pi/tcl-temp -q
+cp -r /home/pi/tcl-temp/* /home/pi/tcl/walking-pi-bot-2/
+rm -r -f /home/pi/tcl-temp/* 2>> ~/errors
+rm -r -f /home/pi/tcl-temp/.* 2>> ~/errors
 
